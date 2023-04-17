24780c28dee12bfc624e8a547659acf72ee83e0d
rootlex <59170489+rootlexblog@users.noreply.github.com>
2023-04-17 21:24:33 +0800

    ✨ feat: 增加自动写入摘要到文章

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const strip = require('./strip')
 const log = require('hexo-log')()
 const ai = require('./ai')
+const fs = require('hexo-fs')
+const fm = require('hexo-front-matter')
 
 const config = hexo.config.aiexcerpt
 hexo.extend.filter.register('after_post_render', async function (data) {
@@ -11,7 +13,10 @@ hexo.extend.filter.register('after_post_render', async function (data) {
         log.info(`文章 ${data.title} 超过max_token限制`);
         return data
     }
-    data.excerpt = await ai(config.token, config.api, content, config.prompt, config.max_token)
     log.info(`摘要 ${data.title} 完成`);
+    const path = this.source_dir + data.source
+    const frontMatter = fm.parse(await fs.readFile(path));
+    frontMatter.excerpt = data.excerpt = await ai(config.custom, config.token, config.api, config.model, content, config.prompt, config.max_token)
+    await fs.writeFile(path, `---\n${fm.stringify(frontMatter)}---\n${data.content}`);
     return data
 });
